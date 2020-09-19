a3c52e49cf5da577a504628dda4f0133227fcc97
Jonas <jonasshansen@gmail.com>
2020-09-20 00:09:12 +0200

    removed comment

--- src/server/index.js
+++ src/server/index.js
@@ -24,7 +24,7 @@ app.get("/", (req, res, next) => {
           <!DOCTYPE html>
           <html>
             <head>
-              <title>SSR with RR</title>
+              <title>Weather App</title>
               <script src="/bundle.js" defer></script>
               <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
               <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -42,17 +42,6 @@ app.get("/weather", (req, res) => {
   fetchWeather(req.query.city).then((data) => {
     res.send(data);
   });
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // const city = req.query.city;
-  // const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},dk&appid=${weatherAPIKey}&units=metric&lang=dk`;
-  // fetch(url)
-  //   .then((res) => res.json())
-  //   .then((response) => {
-  //     res.send(response);
-  //   })
-  //   .catch((err) => {
-  //     res.send(err);
-  //   });
 });
 
 app.listen(3000, () => {
